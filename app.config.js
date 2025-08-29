10f49e4c7b627db8a6363e51ffaef15e0f90d771
BlauBeerToni84 <BlauBeerToni84@users.noreply.github.com>
2025-08-29 12:55:54 +0200

    chore: initial push

---
+++ app.config.js
@@ -0,0 +1,7 @@
+export default ({ config }) => ({
+  ...config,
+  name: "k1w1 APK Builder",
+  slug: "k1w1-apk-builder",
+  icon: "./assets/icon.png",
+  android: { adaptiveIcon: { foregroundImage: "./assets/icon.png", backgroundColor: "#FFFFFF" } }
+});
