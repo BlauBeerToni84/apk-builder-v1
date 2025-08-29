c9ee706f65204c175c9d11198e9035398b30d69f
BlauBeerToni84 <BlauBeerToni84@users.noreply.github.com>
2025-08-29 17:58:52 +0200

    chore: stable expo53 + setup-android workflow

--- app.config.js
+++ app.config.js
@@ -3,5 +3,8 @@ export default ({ config }) => ({
   name: "k1w1 APK Builder",
   slug: "k1w1-apk-builder",
   icon: "./assets/icon.png",
-  android: { adaptiveIcon: { foregroundImage: "./assets/icon.png", backgroundColor: "#FFFFFF" } }
+  android: {
+    package: "com.anonymous.k1w1apkbuilder",
+    adaptiveIcon: { foregroundImage: "./assets/icon.png", backgroundColor: "#FFFFFF" }
+  }
 });
