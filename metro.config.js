34e2095b9f6c1aa7dc364e99611b3371fdc7cbde
BlauBeerToni84 <BlauBeerToni84@users.noreply.github.com>
2025-09-03 19:33:56 +0200

    build(metro): alias '@' -> './src'

--- metro.config.js
+++ metro.config.js
@@ -1,31 +1,14 @@
 const path = require('path');
-
 let getDefaultConfig;
-try {
-  ({ getDefaultConfig } = require('@react-native/metro-config'));
-} catch (e) {
-  ({ getDefaultConfig } = require('expo/metro-config'));
-}
+try { ({ getDefaultConfig } = require('@react-native/metro-config')); }
+catch { ({ getDefaultConfig } = require('expo/metro-config')); }
 
-const { resolver, transformer, serializer, server, symbolicator } = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-// Standard-Resolver von Metro:
-const { resolve } = require('metro-resolver');
-
-module.exports = {
-  resolver: {
-    ...resolver,
-    // Handle "@/foo/bar" -> "<repo>/src/foo/bar"
-    resolveRequest: (context, moduleName, platform) => {
-      if (moduleName.startsWith('@/')) {
-        const mapped = path.join(__dirname, 'src', moduleName.slice(2));
-        return resolve(context, mapped, platform);
-      }
-      return resolve(context, moduleName, platform);
-    },
-  },
-  transformer,
-  serializer,
-  server,
-  symbolicator,
+// Alias '@' -> '<root>/src' (deckt '@/x/y' automatisch ab)
+config.resolver = {
+  ...(config.resolver || {}),
+  alias: { ...(config.resolver?.alias || {}), '@': path.resolve(__dirname, 'src') },
 };
+
+module.exports = config;
